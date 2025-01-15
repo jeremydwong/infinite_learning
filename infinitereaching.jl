59f14ad4b46fb77a44e5f7d75d6296b42e62d7d6
jeremy wong <jeremydwong@gmail.com>
2025-01-15 14:41:02 -0700

    initial.

--- infinitereaching.jl
+++ infinitereaching.jl
@@ -1,7 +1,48 @@
+# note: this is originally modelling from Art Kuo, 
+# I may have inadvertently edited, but was using for reference.
 # Infinite reaching
 
 using InfiniteOpt, Ipopt, Plots, Distributions
 include("plotpvu.jl")
+# Minimum jerk/variance/fdot point mass model
+# Arbitrary third-order model (with damping) is sufficient to 
+# produce bell-shaped trajectories with fdot squared
+# extra damping affects the actuation but not the motion
+
+hand = InfiniteModel(Ipopt.Optimizer)
+@infinite_parameter(hand, t in [0, 1], num_supports=40, derivative_method = OrthogonalCollocation(2))
+@finite_parameter(hand, B == 0)
+@variable(hand, p, Infinite(t))
+@variable(hand, v, Infinite(t))
+@variable(hand, F, Infinite(t))
+@variable(hand, Fdot, Infinite(t))
+
+@objective(hand, Min, integral(Fdot.^2,t))
+
+@constraint(hand, deriv(p,t) == v)   # triple integrator with damping
+@constraint(hand, deriv(v,t) == -B*v + F)
+@constraint(hand, deriv(F,t) == Fdot)
+
+@constraint(hand, p(0) == 0)
+@constraint(hand, p(1) == 1)
+@constraint(hand, v(0) == 0)
+@constraint(hand, v(1) == 0)
+@constraint(hand, F(0) == 0)
+@constraint(hand, F(1) == 0)
+
+optimize!(hand)
+
+pfig = plot(layout=(1,2))
+plot!(pfig[1],supports(t), value(v))
+plot!(pfig[2], supports(t), value(F))
+
+pfig = plot()
+for Bval in 0:0.2:2
+    set_value( B, Bval)
+    optimize!(hand)
+    plotpvu!(pfig, t, (p, v,F)..., title="B = $Bval")
+end
+display(pfig)
 
 ## Third order eye model Harris & Wolpert
 # three time constants, 224, 13, 10 ms
@@ -27,7 +68,7 @@ eyesdn = InfiniteModel(Ipopt.Optimizer)
 
 @constraint(eyesdn, deriv(p,t) == v) # Dynamics
 @constraint(eyesdn, deriv(v,t) == a)
-@constraint(eyesdn, deriv(a,t) == -eyeden[0]/eyeden[3]*p - eyeden[1]/eyeden[3]*v - eyeden[2]/eyeden[3]*a + 1/eyeden[3]*u)
+@constraint(eyesdn, deriv(a,t) == -eyeden[0]/eyeden[3]*p -eyeden[1]/eyeden[3]*v - eyeden[2]/eyeden[3]*a + 1/eyeden[3]*u*(1+8*w))
 
 @constraint(eyesdn, p(0,w) == 0) # Boundary conditions
 @constraint(eyesdn, expect(p(th,w),w) == 1)
